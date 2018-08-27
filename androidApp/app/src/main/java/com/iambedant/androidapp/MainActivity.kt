f99bee6a071839e92e96617ac734377b12ef1047
kuliza <kuliza@Kuliza-358s-MacBook-Pro.local>
2018-08-27 15:59:37 +0530

    feat: Android working

---
+++ androidApp/app/src/main/java/com/iambedant/androidapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.iambedant.androidapp
+import org.greeting.*
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = Greeting().greeting()
+    }
+}
---
+++ greeting/ios/src/main/kotlin/ios.kt
@@ -0,0 +1,6 @@
+// greeting/ios/src/main/kotlin/ios.kt
+package org.greeting
+
+actual class Platform actual constructor() {
+    actual val platform: String = "iOS"
+}
